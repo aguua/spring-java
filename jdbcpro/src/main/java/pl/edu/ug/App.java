9cec728cf61a3b322ae08111da757f6eed0d22d4
Agnieszka Harłozińska <aharlozinska@INF.UG.EDU.PL>
2019-10-29 13:42:08 +0100

    lab2 dodanie PreparedStatement

--- jdbcpro/src/main/java/pl/edu/ug/App.java
+++ jdbcpro/src/main/java/pl/edu/ug/App.java
@@ -14,62 +14,84 @@ public class App {
     final String UPDATE_OSOBA_WAGA = "UPDATE OSOBA SET WAGA =";
     final String DROP_TABLE = "DROP TABLE OSOBA";
     
-
-    Connection connection = DriverManager.getConnection(DB_URL);
-    
-    
-    //CREATE TABLE 
-    Statement st_new = connection.createStatement();
-    ResultSet rs_new = connection.getMetaData().getTables(null, null, null, null);
-
-    boolean tableExists = false;
-
-    while (rs_new.next()) {
-      if ("Osoba".equalsIgnoreCase(rs_new.getString("TABLE_NAME"))) {
-        tableExists = true;
-        break;
-      }
+    try {
+	    Connection connection = DriverManager.getConnection(DB_URL);
+	    
+	    
+	    //CREATE TABLE 
+	    Statement st_new = connection.createStatement();
+	    ResultSet rs_new = connection.getMetaData().getTables(null, null, null, null);
+	
+	    boolean tableExists = false;
+	
+	    while (rs_new.next()) {
+	      if ("Osoba".equalsIgnoreCase(rs_new.getString("TABLE_NAME"))) {
+	        tableExists = true;
+	        break;
+	      }
+	    }
+	
+	    if (!tableExists)
+	      st_new.executeUpdate(CREATE_TABLE_OSOBA);
+	    
+	    //INSERT 
+	    Statement st_insert = connection.createStatement();
+	    st_insert.executeQuery(INSERT_OSOBA + "(1, 'Agnieszka', 'Graj', 55.1), (2, 'Paweł', 'Witkowski', 87.21), (3, 'Maja', 'Swatek', 43.2)");
+	    
+	    //READ DATA FROM TABLE
+	    Statement st = connection.createStatement();
+	    ResultSet rs = st.executeQuery(SELECT_ALL_OSOBA);
+	    ResultSetMetaData rsmd = rs.getMetaData();
+	
+	    int columnsNumber = rsmd.getColumnCount();
+	    while (rs.next()) {
+	    	int nr = rs.getInt("nr");
+	    	String imie = rs.getString("imie");
+	    	double waga = rs.getDouble("waga");
+	    	  System.out.println(nr + "  | " +imie+" | "+ waga+ "\n");
+	    	}
+	    
+	    //UPDATE TABLE
+	    Statement st_up = connection.createStatement();
+	    st_up.executeQuery(UPDATE_OSOBA_WAGA+ "50 where imie = 'Maja'");
+	    
+	    //READ DATA FROM TABLE
+	    while (rs.next()) {
+	    	int nr = rs.getInt("nr");
+	    	String imie = rs.getString("imie");
+	    	double waga = rs.getDouble("waga");
+	    	  System.out.println(nr + "  | " +imie+" | "+ waga+ "\n");
+	    	}
+	
+	    //DROP TABLE
+	    //Statement st_drop = connection.createStatement();
+	    //ResultSet rs_drop = st_drop.executeQuery(DROP_TABLE);
+	    
+	    
+	    //PreparedStatement 
+	    
+	    PreparedStatement pstmt_create = connection.prepareStatement("CREATE TABLE ? (?, ?)");
+	    	pstmt_create.setString(1, "NOWA");
+	    	pstmt_create.setString(2, "nr int ");
+	    	pstmt_create.setString(3, "imie varchar(30)");
+    		pstmt_create.executeUpdate();
+    		pstmt_create.close();
+	    	
+	    
+	    PreparedStatement pstmt = connection.prepareStatement("INSERT INTO OSOBA VALUES (?,?,?,?)");
+	    		pstmt.setInt(1, 8);
+	    		pstmt.setString(2, "Marek");
+	    		pstmt.setString(3, "Czarek");
+	    		pstmt.setDouble(4, 87.22);
+	    		pstmt.executeUpdate();
+	    		pstmt.close();
+	    		
+    } catch (SQLException e) {
+        e.printStackTrace();
     }
+    		
 
-    if (!tableExists)
-      st_new.executeUpdate(CREATE_TABLE_OSOBA);
-    
-    //INSERT 
-    Statement st_insert = connection.createStatement();
-    st_insert.executeQuery(INSERT_OSOBA + "(1, 'Agnieszka', 'Graj', 55.1), (2, 'Paweł', 'Witkowski', 87.21), (3, 'Maja', 'Swatek', 43.2)");
-    
-    //READ DATA FROM TABLE
-    Statement st = connection.createStatement();
-    ResultSet rs = st.executeQuery(SELECT_ALL_OSOBA);
-    ResultSetMetaData rsmd = rs.getMetaData();
 
-    int columnsNumber = rsmd.getColumnCount();
-    while (rs.next()) {
-    	int nr = rs.getInt("nr");
-    	String imie = rs.getString("imie");
-    	double waga = rs.getDouble("waga");
-    	  System.out.println(nr + "  | " +imie+" | "+ waga+ "\n");
-    	}
-    
-    //UPDATE TABLE
-    Statement st_up = connection.createStatement();
-    st_up.executeQuery(UPDATE_OSOBA_WAGA+ "50 where imie = 'Maja'");
-    
-    //READ DATA FROM TABLE
-    while (rs.next()) {
-    	int nr = rs.getInt("nr");
-    	String imie = rs.getString("imie");
-    	double waga = rs.getDouble("waga");
-    	  System.out.println(nr + "  | " +imie+" | "+ waga+ "\n");
-    	}
-
-    //DROP TABLE
-    Statement st_drop = connection.createStatement();
-    ResultSet rs_drop = st_drop.executeQuery(DROP_TABLE);
-    
-
-    
-       
      
   }
  
