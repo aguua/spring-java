744fab872b6127af3dd3bad6123487318a7a0719
Agnieszka <aguua5@gmail.com>
2019-11-21 22:17:31 +0100

    connection with database wypozyczalnia

--- wypozyczalnia-aut-projekt/src/main/java/pl/edu/ug/App.java
+++ wypozyczalnia-aut-projekt/src/main/java/pl/edu/ug/App.java
@@ -1,13 +1,31 @@
 package pl.edu.ug;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+    
+	public static Connection GetConnection()  throws SQLException {
+		final String DB_URL = "jdbc:hsqldb:hsql://localhost/wypozyczalnia";
+		try{
+			DriverManager.getConnection(DB_URL);
+		}catch (SQLException e) {
+	        e.printStackTrace();
+		}
+		return DriverManager.getConnection(DB_URL);
+	}
+	
+	
+    public static void main( String[] args ) throws SQLException
     {
         System.out.println( "Hello World!" );
+        Connection con = GetConnection();
     }
 }
