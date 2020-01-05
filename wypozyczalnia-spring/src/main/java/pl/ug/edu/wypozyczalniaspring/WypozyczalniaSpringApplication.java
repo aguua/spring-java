0f168b92ec50550f4f60890073effdbfbead7848
Agnieszka <aguua5@gmail.com>
2020-01-05 11:25:12 +0100

    init spring project wypozyczalnia

---
+++ wypozyczalnia-spring/src/main/java/pl/ug/edu/wypozyczalniaspring/WypozyczalniaSpringApplication.java
@@ -0,0 +1,13 @@
+package pl.ug.edu.wypozyczalniaspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WypozyczalniaSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WypozyczalniaSpringApplication.class, args);
+	}
+
+}
---
+++ wypozyczalnia-spring/src/test/java/pl/ug/edu/wypozyczalniaspring/WypozyczalniaSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.ug.edu.wypozyczalniaspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WypozyczalniaSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
