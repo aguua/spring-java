56d47000697ade309f377c3f1aa9b6ff00ed0c60
Agnieszka <aguua5@gmail.com>
2020-01-05 13:15:25 +0100

    sql mode  with insert

--- wypozyczalnia-spring/src/main/resources/data.sql
+++ wypozyczalnia-spring/src/main/resources/data.sql
@@ -5,4 +5,8 @@ INSERT INTO klient (imie,nazwisko) VALUES ('Michelle', 'Dessler');
 
 INSERT INTO auto (nr_rejestracyjny, cena, model, marka, typ_paliwa) VALUES ('CCH56H21', 40.5, 'Ford', 'Focus', 'diesel');
 INSERT INTO auto (nr_rejestracyjny, cena, model, marka, typ_paliwa) VALUES ('GDA45GT1', 25.5, 'Ford', 'Fiesta', 'benzyna');
-INSERT INTO auto (nr_rejestracyjny, cena, model, marka, typ_paliwa) VALUES ('GD54D66A', 40.0, 'Opel', 'Corsa', 'gaz')
+INSERT INTO auto (nr_rejestracyjny, cena, model, marka, typ_paliwa) VALUES ('GD54D66A', 40.0, 'Opel', 'Corsa', 'gaz');
+
+INSERT INTO wypozyczenie (data_wypozyczenia, data_zwrotu, klient_id_klient) VALUES ('10-03-2019', '29-03-2019', 1);
+
+INSERT INTO wypozyczenie_auta(wypozyczenie_id_wypozyczenie, auta_id_auto) VALUES (0, 2), (0,1);
